458cb85d21799ba1f058ea5927dac11ba1a6f59f
yamanoha-hab <r12m097@yahoo.co.jp>
2021-12-29 20:17:18 +0900

    index.html レスポンシブ作成

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 'use stript';
 
 $(function() {
+
+  // ハンバーガーメニュー
+  $('.hamburger').click(function() {
+    $(this).toggleClass('hamburger__active');
+    if($('.hamburger').hasClass('hamburger__active')) {
+      $('.header__nav').show();
+      // $('.sub-header__nav').show();
+    } else {
+      $('.header__nav').hide();
+      // $('.sub-header__nav').hide();
+    }
+  });
+
+
+  // アコーディオン
   $('.faq__box__item').click(function() {
     $(this).children('.accordion').slideToggle();
     $(this).toggleClass('on');
     return false;
   });
+
+
   
 
 });
